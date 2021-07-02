643cbf911d713f773afadcdc875c60865861dc7c
Jordan Davies <Jordan.Davies@castletonplc.com>
2021-07-02 11:26:31 +0100

    Added tabs page.

---
+++ lib/calendar/calendar_tabs_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meal_planner/calendar/pages/day_page.dart';
+import 'package:meal_planner/calendar/pages/week_page.dart';
+
+class CalendarTabsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTabController(
+          length: 2,
+          child: Column(
+            children: [
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    DayPage(),
+                    WeekPage(),
+                  ],
+                ),
+              ),
+              Container(
+                child: TabBar(
+                  tabs: [
+                    Tab(text: "Day"),
+                    Tab(text: "Week"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/meal_planner_app.dart
+++ lib/meal_planner_app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:meal_planner/calendar/calendar_tabs_page.dart';
+import 'package:meal_planner/themes.dart';
 
 class MealPlannerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Container());
+    return MaterialApp(
+      theme: defaultTheme,
+      home: CalendarTabsPage(),
+    );
   }
 }
