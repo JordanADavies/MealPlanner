77590e03f6a01a14b8768548d928de00de0b55b7
Jordan Davies <Jordan.Davies@castletonplc.com>
2021-07-02 11:17:02 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:meal_planner/meal_planner_app.dart';
+
+void main() => runApp(MealPlannerApp());
---
+++ lib/meal_planner_app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MealPlannerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Container());
+  }
+}
